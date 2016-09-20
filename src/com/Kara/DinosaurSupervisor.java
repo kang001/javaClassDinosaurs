b7ea1dfd4d396c4a6756e78f6668423b810efacf
cats <cats@jellybeans.com>
2016-09-20 11:39:34 -0500

    Added even more comments

--- src/com/Kara/DinosaurSupervisor.java
+++ src/com/Kara/DinosaurSupervisor.java
@@ -5,11 +5,12 @@ package com.Kara;
  */
 public class DinosaurSupervisor {
     public static void main(String[] args) {
-
+        //make new objects from dinosaur class
         Dinosaur t_rex = new Dinosaur();
         Dinosaur velociraptor = new Dinosaur();
         Dinosaur diplodocus = new Dinosaur();
         Dinosaur dude = new Dinosaur();
+
 
 
         t_rex.roar();
